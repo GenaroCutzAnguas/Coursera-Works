7a81d40725aa6a881fe08a8c5e735805b01b0df6
Genaro Cutz <carlos.genaro.cutz.anguas@gmail.com>
2023-12-14 23:06:22 -0600

    programa simple de python

---
+++ String.py
@@ -0,0 +1,12 @@
+abc = "ejemplo de division de cadenas"
+cadenaSplit = abc.split()
+print(cadenaSplit)
+x = list(range(5))
+print (x)
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(len(c))
+
+stuff = dict()
+print(stuff.get('candy',-1))
